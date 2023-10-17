ae9b99158239b301ea0023a84ca26bd6d26dfb98
Christinne <vanessa0christinne@gmail.com>
2023-10-18 01:14:52 +0200

    update

--- basic_ex/multiply.s
+++ basic_ex/multiply.s
@@ -6,23 +6,20 @@
 .global _start
 
 _start:
-    // Inicializa 'num' com o valor 5
     ldr x1, =num
-
-    // Carrega o valor de 'num' em 'x2' (result)
     ldr x2, [x1]
 
     // Multiplica 'result' por 2
     lsl x2, x2, #1  // Multiplica x2 por 2 (deslocamento à esquerda por 1 bit)
 
-    // Imprime o valor de 'result'
-    mov x0, #1        // File descriptor 1 (stdout)
-    ldr x1, =result   // Endereço do valor a ser impresso
-    ldr x2, [x1]      // Carrega o valor de memória no registrador x2
-    mov x8, #64       // Número da syscall para sys_write
-    svc #0            // Chama a syscall
+    // print result
+    mov x0, #1        
+    ldr x1, =result             // endereço do valor a ser impresso
+    ldr x2, [x1]                // carrega o valor de memória no registrador x2
+    mov x8, #64                 // sys_write
+    svc #0                      
 
-    // Encerra o programa
+    // exit
     mov x0, #0
-    mov x8, #93       // Número da syscall para sys_exit
+    mov x8, #93                 
     svc #0
