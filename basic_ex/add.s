e8def0a0ae32541fba12eea7a3fa8b112ffa1091
Christinne <vanessa0christinne@gmail.com>
2023-10-17 20:58:01 +0200

    update

--- basic_ex/add.s
+++ basic_ex/add.s
@@ -1,27 +1,22 @@
 .data
-    num:    .word   5       // Reserva 4 bytes para 'num' e inicializa com 5
-    result: .word   0       // Reserva 4 bytes para 'result', inicializado com 0
+    num:    .word   5       
+    result: .word   0       
 
 .text
 .global _start
 
 _start:
-    // Inicializa 'num' com o valor 5
-    ldr x1, =num
+    ldr x1, =num                // inicializa 'num'
+    ldr x2, [x1]                // carrega o valor de 'num' em 'x2' (result)
+    add x2, x2, #1              // adiciona 1 a 'result'
 
-    // Carrega o valor de 'num' em 'x2' (result)
-    ldr x2, [x1]
+    // print 'result'
+    mov x8, #64          // ssys_write
+    ldr x3, =1           // stdout
+    mov x4, x2           // print result
+    svc #0               // syscall
 
-    // Adiciona 1 a 'result'
-    add x2, x2, #1
-
-    // Imprime o valor de 'result'
-    mov x8, #64          // Número da syscall para sys_write
-    ldr x3, =1           // File descriptor 1 (stdout)
-    mov x4, x2           // Valor a ser impresso (result)
-    svc #0               // Chama a syscall
-
-    // Encerra o programa
+    // encerra o programa
     mov x0, #0
-    mov x8, #93          // Número da syscall para sys_exit
+    mov x8, #93          // sys_exit
     svc #0
