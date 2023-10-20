743a5318fa698728802f859c6a5bd640f0e123c8
Christinne <vanessa0christinne@gmail.com>
2023-10-21 01:11:13 +0200

    not fix at all

--- basic_ex/add.s
+++ basic_ex/add.s
@@ -7,19 +7,18 @@
 
 _start:
     ldr x1, =num                
+    ldr x2, [x1]        // carrega o valor de 'num' em 'x2' (result)
+    add x2, x2, #1      // adiciona 1 a 'result'
 
-    // carrega o valor de 'num' em 'x2' (result)
-    ldr x2, [x1]
-
-    // adiciona 1 a 'result'
-    add x2, x2, #1
+    mov x0, #1
+    mov x1, x2           
+    mov x2, #4
 
     mov x8, #64          // sys_write
-    ldr x3, =1           // stdout
-    mov x4, x2           
-    svc #0               
+    svc #0            
 
     // encerra o programa
     mov x0, #0
     mov x8, #93          
     svc #0
+    
