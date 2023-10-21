f946be08980868be788b6ed96d1817f671df0027
Christinne <vanessa0christinne@gmail.com>
2023-10-21 17:45:31 +0200

    still dont work

--- add_ex/sum.s
+++ add_ex/sum.s
@@ -1,23 +1,31 @@
-.data
-    num:    .word   5       
-    result: .word   0       
-
 .text
 .global _start
 .align 2
 
 _start:
-    mov x0, #1       
-    mov x1, #2
-    bl _add  
-    mov x2, x0
-    mov x16, #64             // sys_write
-    b _terminate
+    mov x0, #2                  // first number
+    mov x1, #2                  // second number
 
-_add:
-    add x0, x0, x1
+    bl add_numbers
+    bl _printf
+    bl _terminate
+
+_printf:
+    mov x0, #1                  // stdout
+    adr x1, result_string       // endereço da string de resultado
+    mov x2, #11                 // string length
+    mov x16, #4                 // sys_write
+    svc 0                       // syscall
+    ret
 
 _terminate:
-    mov x0, #0
-    mov x16, #1          
+    mov x0, #0                  
+    mov x16, #1            
     svc 0
+    ret                  
+
+add_numbers:
+    add x0, x0, x1
+    ret
+
+result_string: .ascii "Result: %d\n"
